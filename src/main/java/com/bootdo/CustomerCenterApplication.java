7312a1b4810fb1a9e5c3d7acba6cf28b1de21583
anyalei123 <anyalei@sinosoft.com.cn>
2018-07-30 10:44:23 +0800

    九恒仁福客户中心项目立项

---
+++ src/main/java/com/bootdo/CustomerCenterApplication.java
@@ -0,0 +1,22 @@
+package com.bootdo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@ServletComponentScan
+@MapperScan("com.bootdo.*.dao")
+@SpringBootApplication
+@EnableCaching
+public class CustomerCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerCenterApplication.class, args);
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    九恒仁福P03客户中心---启动成功      ヾ(◍°∇°◍)ﾉ");
+        
+    }
+}
