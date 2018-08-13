fb5620a372e673a40400d19e3b61b20f04848533
zhaolijuan <zhaolijuan@sinosoft.com.cn>
2018-08-13 14:10:12 +0800

    模板管理dao

---
+++ src/main/java/com/bootdo/template/dao/PlaceholderDao.java
@@ -0,0 +1,67 @@
+package com.bootdo.template.dao;
+
+import com.bootdo.template.domain.PlaceholderDO;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 占位符管理
+ *
+ * @author zhaolijuan
+ * @date 2018-08-02 11:40:43
+ */
+@Mapper
+public interface PlaceholderDao {
+
+	/**
+	 * 根据占位符id查询占位符
+	 * @param placeholderId
+	 * @return
+	 */
+	PlaceholderDO get(String placeholderId);
+
+	/**
+	 * 查询占位符列表
+	 * @param map
+	 * @return
+	 */
+	List<PlaceholderDO> list(Map<String, Object> map);
+
+	/**
+	 * 查询占位符列表数据总条数
+	 * @param map
+	 * @return
+	 */
+	int count(Map<String, Object> map);
+
+	/**
+	 * 保存占位符信息
+	 * @param placeholder
+	 * @return
+	 */
+	int save(PlaceholderDO placeholder);
+
+	/**
+	 *修改占位符信息
+	 * @param placeholder
+	 * @return
+	 */
+	int update(PlaceholderDO placeholder);
+
+	/**
+	 * 根据id删除占位符
+	 * @param placeholderId
+	 * @return
+	 */
+	int remove(String placeholderId);
+
+	/**
+	 * 批量删除占位符
+	 * @param placeholderIds
+	 * @return
+	 */
+	int batchRemove(String[] placeholderIds);
+}
