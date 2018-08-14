35bc6d3c469b601c5f1521923d03ae8e634f91d1
anyalei123 <anyalei@sinosoft.com.cn>
2018-08-14 14:22:49 +0800

    注释国际化拦截器代码

--- src/main/java/com/bootdo/common/utils/LocaleConfig.java
+++ src/main/java/com/bootdo/common/utils/LocaleConfig.java
@@ -37,23 +37,25 @@ public class LocaleConfig extends WebMvcConfigurerAdapter {
         lci.setParamName("lang");
         return lci;
     }
-    @Bean
+    
+    //去掉国际化拦截器代码
+  /*  @Bean
     public LocaleInterceptor localeInterceptor(){
         return new LocaleInterceptor();
     }
-
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-        registry.addInterceptor(new LocaleInterceptor()).addPathPatterns("/**")
-                .excludePathPatterns("/**/fonts/*").excludePathPatterns("/**/*.css")
-                .excludePathPatterns("/**/*.js").excludePathPatterns("/**/*.jpg")
-                .excludePathPatterns("/**/*.png").excludePathPatterns("/**/*.gif*")
-                .excludePathPatterns("/**/*.jpeg").excludePathPatterns("/login")
-                .excludePathPatterns("/common/dict/type")
-                .excludePathPatterns("/common/dict").excludePathPatterns("/oa/notify/message");
-
-    }
+*/
+  //去掉国际化拦截器代码
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(localeChangeInterceptor());
+//        registry.addInterceptor(new LocaleInterceptor()).addPathPatterns("/**")
+//                .excludePathPatterns("/**/fonts/*").excludePathPatterns("/**/*.css")
+//                .excludePathPatterns("/**/*.js").excludePathPatterns("/**/*.jpg")
+//                .excludePathPatterns("/**/*.png").excludePathPatterns("/**/*.gif*")
+//                .excludePathPatterns("/**/*.jpeg").excludePathPatterns("/login")
+//                .excludePathPatterns("/common/dict/type")
+//                .excludePathPatterns("/common/dict").excludePathPatterns("/oa/notify/message");
+//
+//    }
 
 }
