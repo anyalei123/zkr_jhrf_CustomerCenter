7c7df2b1323ec266e8898c49d88cc76148476f7f
zhaolijuan <zhaolijuan@sinosoft.com.cn>
2018-08-24 17:28:28 +0800

    增加“通过名称查询对象”，“查询类型下拉选项”，“根据id删除”，“批量删除”方法

--- src/main/java/com/bootdo/config/dao/self/ConfigSelfDao.java
+++ src/main/java/com/bootdo/config/dao/self/ConfigSelfDao.java
@@ -1,5 +1,6 @@
 package com.bootdo.config.dao.self;
 
+import com.bootdo.config.domain.DictionaryDO;
 import com.bootdo.config.domain.DictionaryTypeDO;
 
 import java.util.List;
@@ -24,6 +25,33 @@ public interface ConfigSelfDao {
 	 * @create 2018-08-21 18:42:27
 	 */
 	DictionaryTypeDO getByTypeName(DictionaryTypeDO dictionaryTypeDO);
+
+	/**
+	 * 通过字典名称查询字典对象
+	 * @param dictionaryDO
+	 * @return
+	 */
+	DictionaryDO getByDictName(DictionaryDO dictionaryDO);
+
+    /**
+     * 查询字典类型下拉选项
+     * @return
+     */
+	List<DictionaryTypeDO> listType();
+
+	/**
+	 * 根据id删除字典类型，若该类型下有数据，类型不能删除
+	 * @param typeId
+	 * @return
+	 */
+	int removeById(String typeId);
+
+	/**
+	 * 批量删除字典类型，若该类型有数据，类型不能删除
+	 * @param typeIds
+	 * @return
+	 */
+	int batchRemove1(String[] typeIds);
 	
 	
 }
