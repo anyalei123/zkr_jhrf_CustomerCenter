1d49815beec52d8d1ac1069ab997a58f6494775a
zhaolijuan <zhaolijuan@sinosoft.com.cn>
2018-08-24 17:30:38 +0800

    增加“查询字典类型下拉选项”，“根据id删除”，“批量删除”的方法

--- src/main/java/com/bootdo/config/service/DictionaryTypeService.java
+++ src/main/java/com/bootdo/config/service/DictionaryTypeService.java
@@ -14,7 +14,6 @@ import java.util.Map;
  */
 public interface DictionaryTypeService {
 	
-	
 	/**
 	 * 通过字典类型名称查询对象
 	 * @param
@@ -23,8 +22,27 @@ public interface DictionaryTypeService {
 	 * @create 2018-08-21 18:42:27
 	 */
 	DictionaryTypeDO getByTypeName(DictionaryTypeDO dictionaryType);
-	
-	
+
+	/**
+	 * 查询字典类型下拉选项
+	 * @return
+	 */
+	List<DictionaryTypeDO> listType();
+
+	/**
+	 * 根据id删除字典类型，若该类型下有数据，类型不能删除
+	 * @param typeId
+	 * @return
+	 */
+	int removeById(String typeId);
+
+	/**
+	 * 批量删除字典类型，若该类型有数据，类型不能删除
+	 * @param typeIds
+	 * @return
+	 */
+	int batchRemove1(String[] typeIds);
+
 	DictionaryTypeDO get(String typeId);
 	
 	List<DictionaryTypeDO> list(Map<String, Object> map);
