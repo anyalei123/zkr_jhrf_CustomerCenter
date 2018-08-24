f7a8ff8482f52ba57bb50ab5bddcdd9471655bd7
zhaolijuan <zhaolijuan@sinosoft.com.cn>
2018-08-24 17:36:25 +0800

    字典类型的回显，删除标记的回显格式化

--- src/main/resources/static/js/appjs/config/dictionary/edit.js
+++ src/main/resources/static/js/appjs/config/dictionary/edit.js
@@ -1,5 +1,33 @@
 $().ready(function() {
 	validateRule();
+    //字典类型的回显
+    $.ajax({
+        type : "GET",
+        url : "/config/dictionaryType/listType",
+        dataType : "json",
+        async : false,
+        success : function(data) {
+            var select = $("#typeId");
+            var value = select.val();
+            $(data).each(function (index, type) {
+                if (type.typeId == value){
+                    $("#typeIdOption").text(type.typeName);
+                }else{
+                    select.append("<option value="+type.typeId+">"+type.typeName+"</option>");
+                }
+            })
+        }
+    });
+    //删除标记回显
+    var delSelect = $("#delFlag");
+	var delFlag = delSelect.val();
+	if(delFlag == "0"){
+		$("#delOption").text("未删除");
+        delSelect.append("<option value='1'>已删除</option>");
+	}else if(delFlag == "1"){
+        $("#delOption").text("已删除");
+        delSelect.append("<option value='0'>未删除</option>");
+	}
 });
 
 $.validator.setDefaults({
