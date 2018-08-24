26853049258d62df678dfbf738fc84e15480b0b5
zhaolijuan <zhaolijuan@sinosoft.com.cn>
2018-08-24 17:26:35 +0800

    保存和修改前去除空格，删除时判断类型下是否有数据，添加根据id查询方法

--- src/main/java/com/bootdo/config/controller/DictionaryTypeController.java
+++ src/main/java/com/bootdo/config/controller/DictionaryTypeController.java
@@ -34,6 +34,7 @@ import com.bootdo.config.service.DictionaryTypeService;
 @Controller
 @RequestMapping("/config/dictionaryType")
 public class DictionaryTypeController extends BaseController {
+
 	@Autowired
 	private DictionaryTypeService dictionaryTypeService;
 	
@@ -76,7 +77,6 @@ public class DictionaryTypeController extends BaseController {
 	@PostMapping("/save")
 	@RequiresPermissions("config:dictionaryType:add")
 	public R save( DictionaryTypeDO dictionaryType){
-		
 		//判断类型名称是否存在
 		DictionaryTypeDO dictionaryType1 = dictionaryTypeService.getByTypeName(dictionaryType);
 		if(dictionaryType1 !=null ){
@@ -84,24 +84,25 @@ public class DictionaryTypeController extends BaseController {
 		}
 		//设置主键
 		dictionaryType.setTypeId(GenerateSequenceUtil.generateSequenceNo());
-		
 		//设置创建、修改信息
 		dictionaryType.setCreateBy(this.getUsername());
 		dictionaryType.setCreateById(String.valueOf(this.getUserId()));
 		dictionaryType.setUpdateBy(this.getUsername());
 		dictionaryType.setUpdateById(String.valueOf(this.getUserId()));
-		
 		//设置时间
 		Date date = new Date();
 		dictionaryType.setCreateTime(date);
 		dictionaryType.setUpdateTime(date);
-		
-		
+		//去除空格
+		dictionaryType.setTypeValue(dictionaryType.getTypeValue().trim());
+		dictionaryType.setTypeName(dictionaryType.getTypeName().trim());
+
 		if(dictionaryTypeService.save(dictionaryType)>0){
 			return R.ok();
 		}
 		return R.error();
 	}
+
 	/**
 	 * 修改
 	 */
@@ -109,13 +110,19 @@ public class DictionaryTypeController extends BaseController {
 	@RequestMapping("/update")
 	@RequiresPermissions("config:dictionaryType:edit")
 	public R update( DictionaryTypeDO dictionaryType){
-		
-		
+		//判断类型名称是否存在
+		DictionaryTypeDO dictionaryType1 = dictionaryTypeService.getByTypeName(dictionaryType);
+		if(dictionaryType1 != null && !dictionaryType1.getTypeId().equals(dictionaryType.getTypeId())){
+			return R.error("字典类型名称  '"+dictionaryType1.getTypeName()+"' 已经存在,请勿重复创建!");
+		}
+		//设置修改信息
 		dictionaryType.setUpdateBy(this.getUsername());
 		dictionaryType.setUpdateById(String.valueOf(this.getUserId()));
-		
 		//设置时间
 		dictionaryType.setUpdateTime(new Date());
+		//去除空格
+		dictionaryType.setTypeValue(dictionaryType.getTypeValue().trim());
+		dictionaryType.setTypeName(dictionaryType.getTypeName().trim());
 		
 		dictionaryTypeService.update(dictionaryType);
 		return R.ok();
@@ -128,21 +135,27 @@ public class DictionaryTypeController extends BaseController {
 	@ResponseBody
 	@RequiresPermissions("config:dictionaryType:remove")
 	public R remove( String typeId){
-		if(dictionaryTypeService.remove(typeId)>0){
-		return R.ok();
+		if(dictionaryTypeService.removeById(typeId)>0){
+			return R.ok();
 		}
-		return R.error();
+		return R.error("该字典类型下有数据，不能删除！");
 	}
-	
+
 	/**
-	 * 删除
+	 * 批量删除
 	 */
 	@PostMapping( "/batchRemove")
 	@ResponseBody
 	@RequiresPermissions("config:dictionaryType:batchRemove")
-	public R remove(@RequestParam("ids[]") String[] typeIds){
-		dictionaryTypeService.batchRemove(typeIds);
-		return R.ok();
+	public R remove(@RequestParam("ids[]") String[] typeIds,Integer selectRows){
+		//如果删除成功的条数等于选中的条数，说明全部删除成功
+		int removeRows = dictionaryTypeService.batchRemove1(typeIds);
+		if(removeRows == selectRows){
+			return R.ok();
+		}
+		//删除失败的条数
+		int errorRows = selectRows - removeRows;
+		return R.error(errorRows+"条数据删除失败，失败原因：类型下有数据，类型不能删除");
 	}
 	
 	/**
@@ -155,7 +168,30 @@ public class DictionaryTypeController extends BaseController {
 		DictionaryTypeDO dictionaryType = dictionaryTypeService.get(typeId);
 		
 		model.addAttribute("dictionaryType", dictionaryType);
-		System.out.println("1111111111111111111");
 	    return "config/dictionaryType/showDetail";
+	}
+
+	/**
+	 * 查询字典类型下拉选项
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/listType")
+	@RequiresPermissions("config:dictionaryType:dictionaryType")
+	public List<DictionaryTypeDO> listType(){
+		return dictionaryTypeService.listType();
+	}
+
+	/**
+	 * 根据id查询字典类型对象
+	 * @param typeId
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/get/{typeId}")
+	@RequiresPermissions("config:dictionaryType:dictionaryType")
+	public DictionaryTypeDO get(@PathVariable("typeId") String typeId){
+		DictionaryTypeDO dictionaryType = dictionaryTypeService.get(typeId);
+        return dictionaryType;
 	}
 }
