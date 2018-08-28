67b5b7794c8852cc4312b6d708daf2b547291baa
zhaolijuan <zhaolijuan@sinosoft.com.cn>
2018-08-28 10:47:34 +0800

    增加根据字典类型名称查询字典数据，修改注释

--- src/main/java/com/bootdo/config/service/DictionaryService.java
+++ src/main/java/com/bootdo/config/service/DictionaryService.java
@@ -6,20 +6,13 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 字典表
+ * 字典管理
  * 
  * @author anyalei
  * @email anyalei163@163.com
  * @date 2018-08-21 16:11:18
  */
 public interface DictionaryService {
-
-	/**
-	 * 通过字典名称查询字典对象
-	 * @param dictionaryDO
-	 * @return
-	 */
-	DictionaryDO getByDictName(DictionaryDO dictionaryDO);
 	
 	DictionaryDO get(String dictId);
 	
@@ -34,4 +27,18 @@ public interface DictionaryService {
 	int remove(String dictId);
 	
 	int batchRemove(String[] dictIds);
+
+	/**
+	 * 通过字典名称查询字典对象
+	 * @param dictionaryDO
+	 * @return
+	 */
+	DictionaryDO getByDictName(DictionaryDO dictionaryDO);
+
+	/**
+	 * 根据字典类型名称查询字典数据
+	 * @param typeName
+	 * @return
+	 */
+	List<DictionaryDO> getByType(String typeName);
 }
