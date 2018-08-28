0d002fda7c3248cad4adb610e84f72f33648cc27
zhaolijuan <zhaolijuan@sinosoft.com.cn>
2018-08-28 15:02:07 +0800

    增加根据字典数据值查询字典对象

--- src/main/java/com/bootdo/config/service/DictionaryService.java
+++ src/main/java/com/bootdo/config/service/DictionaryService.java
@@ -41,4 +41,11 @@ public interface DictionaryService {
 	 * @return
 	 */
 	List<DictionaryDO> getByType(String typeName);
+
+	/**
+	 * 根据字典数据值查询字典对象
+	 * @param dictValue
+	 * @return
+	 */
+	List<DictionaryDO> getByValue(String dictValue);
 }
